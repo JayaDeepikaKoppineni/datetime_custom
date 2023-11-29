e880776fae88aa80158b76329ad5876763e000e4
JayaDeepikaKoppineni <149893574+JayaDeepikaKoppineni@users.noreply.github.com>
2023-11-28 19:18:36 -0600

    Add files via upload

---
+++ test_datetime_custom.py
@@ -0,0 +1,22 @@
+import unittest
+from datetime_custom import MyDateTime
+
+class TestMyDateTime(unittest.TestCase):
+    def test_iso_format(self):
+        # Your test cases for iso_format method
+        pass
+
+    def test_human_readable_format(self):
+        # Your test cases for human_readable_format method
+        pass
+
+    def test_date_difference(self):
+        # Your test cases for date_difference method
+        pass
+
+    def test_date_from_string(self):
+        # Your test cases for date_from_string method
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
